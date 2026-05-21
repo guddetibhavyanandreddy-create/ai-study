b61eafd959a32bd2958578f15c5d76ff3ff23be1
anand <guddetibhavyanandreddy@gmail.com>
2026-05-21 20:48:27 +0530

    updated version flask

--- appp.py
+++ appp.py
@@ -1,1 +1,13 @@
-print("AI study assiss")
+from flask import Flask , render_template , request
+app = Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def hello_world():
+    response = ""
+    if request.method == 'POST':
+        user_question = request.form['question']
+        # Process the question (e.g., send it to an AI model)
+        # For now, we'll just return the question as a simple response
+        response = "ai says:" + user_question
+    return render_template('indx.html', answer=response)
+if __name__ == '__main__':
+    app.run(debug=True)
